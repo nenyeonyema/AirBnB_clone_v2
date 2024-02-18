519450de3f039bda8459b6a52690ed1751a3b378
nenyeonyema <chinenyeonyema1@gmail.com>
2024-02-18 05:53:56 -0800

    hbnb flask route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Default root page """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ say HBNB """
-    return "HBNB"
+    return 'HBNB'
 
 
-if __name__ == __main__:
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
