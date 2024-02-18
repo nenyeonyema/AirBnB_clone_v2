942a0842f85b37708704ab0af2ef0ee7253d6794
nenyeonyema <chinenyeonyema1@gmail.com>
2024-02-18 12:37:31 -0800

    number route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ a script that starts a Flask web application """
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Says Hello HBNB! """
+    """ displays Hello HBNB """
     return 'Hello HBNB!'
 
 
@@ -18,16 +19,23 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """ displays C and a text with _ replaced """
+    """ displays C followed by a text """
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    """ displays Python and a text with _ replaced with a space """
+    """ displays Python followed by text """
     return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ display n if only it's an integer """
+    return '{} is a number'.format(n)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
