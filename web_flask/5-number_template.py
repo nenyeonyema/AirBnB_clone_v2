1be1d5566ab9178a6a38425915c51ae4f1d6a6ab
nenyeonyema <chinenyeonyema1@gmail.com>
2024-02-18 12:56:00 -0800

    render html file from template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ a script that starts a Flask web application """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -34,6 +34,12 @@ def python_text(text):
 def number(n):
     """ display n if only it's an integer """
     return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """  display a HTML page only if n is an integer """
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
